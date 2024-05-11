8993c65eb14e63f14943557c2a078a772d014c9d
szy.szu <szy.szumski@gmail.com>
2024-05-11 13:45:12 +0200

    created custom hook for app context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,10 +1,5 @@
-import {
-  FC,
-  PropsWithChildren,
-  createContext,
-  useEffect,
-  useState,
-} from "react";
+import { FC, PropsWithChildren, createContext } from "react";
+import { useAppContext } from "./hooks/useAppContext";
 
 export type PostType = {
   id: number;
@@ -28,41 +23,7 @@ export const AppContext = createContext<AppContextType>({} as AppContextType);
 // krok 3 - tworzenie naszego providera z propsami
 
 export const AppContextProvider: FC<PropsWithChildren> = ({ children }) => {
-  const [posts, setPosts] = useState<PostType[]>([]);
-
-  const getPosts = async () => {
-    try {
-      const res = await fetch("https://dummyjson.com/posts");
-
-      if (!res.ok) throw new Error("Cannot fetch posts!");
-
-      const { posts } = await res.json();
-
-      if (posts) setPosts(posts);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const addPost = (post: PostType) => setPosts((prev) => [...prev, post]);
-
-  const updatePost = (updatedPost: PostType) => {
-    setPosts((prev) =>
-      prev.map((post) => {
-        if (updatedPost.id !== post.id) return post;
-
-        return updatedPost;
-      })
-    );
-  };
-
-  const deletePostByID = (id: number) => {
-    setPosts((prev) => prev.filter((post) => post.id !== id));
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
+  const { posts, addPost, updatePost, deletePostByID } = useAppContext();
 
   return (
     <AppContext.Provider
