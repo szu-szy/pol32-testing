a8bf2c4d94e92673923d6c96410efc9a9a169b8c
szy.szu <szy.szumski@gmail.com>
2024-05-11 09:20:21 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./pages/Home/Home";
-import { Contact } from "./pages/Contact/Contact";
 
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />}>
-          <Route path=":id" element={<Contact />} />
-        </Route>
-      </Routes>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
